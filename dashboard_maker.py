303fbb795d4eb23760b86522f815805ffa10daca
AboAbdelhameed <mohammad.abid.p95@gmail.com>
2021-02-08 06:47:27 +0200

    Final Version

--- dashboard_maker.py
+++ dashboard_maker.py
@@ -6,13 +6,11 @@ from summary_data_manager import load_dashboard_data
 from plotly import graph_objects as go
 
 
+
 # load all dashboard data
 dashboard_data = load_dashboard_data( )
-
-
 # implement the dashboard server
 app = dash.Dash( __name__ )
-
 
 
 # a function to make trust words chart figure
@@ -20,7 +18,7 @@ def make_trust_words_fig():
     trust_words_fig = px.bar( dashboard_data['trust_words_data'], x = 'Trust Words', y = 'Appearance Amount', 
                     hover_data = ['Trust Words', 'Appearance Amount'], color = 'Trust Words', 
                     labels = { 'pop': 'Appearance of Trust Words' }, height = 400,
-                    title = 'Appearance of trust words in the given dataset'
+                    title = '<b> 1) Appearance of trust words in the given dataset </b>'
                 )
     return trust_words_fig
 
@@ -30,7 +28,8 @@ def make_emotional_pairs_fig():
     trust_emotional_pairs_fig = px.scatter( 
                                             dashboard_data['trust_emotional_pairs_data'],
                                             x = 'Trust-Emotional Pairs', y = 'Appearance Amount',
-                                            color = 'Trust-Emotional Pairs', title = 'Appearance of the trust-emotional pairs',
+                                            color = 'Trust-Emotional Pairs', 
+                                            title = '<b> 2) Appearance of the trust-emotional pairs </b>',
                                             size='Appearance Amount', hover_data=['Appearance Amount'] 
                                       )
     return trust_emotional_pairs_fig
@@ -57,32 +56,89 @@ def make_emotional_words_fig():
         ))
 
     emotional_words_fig.update_layout(
-            width = 1000,
-            height = 700,
-            title_text = 'Appearence of Emotion Words By Appearence Range',
-            margin = dict( l = 100, r = 100, t = 200, b = 100 )
+            title_text = '<b> 3) Appearence of Emotion Words By Appearence Range</b>',
         )
 
     return emotional_words_fig
 
 
 # a function to make emotion words chart figure
-def make_pie_charts():
-    df = px.data.gapminder().query("continent == 'Asia' and year == 2007")
-    fig = px.pie(df, values='pop', names='country')
-    fig.update_traces(textposition='inside', textfont_size=14)
+def make_existing_emotion_words_fig():
+    fig = px.pie(   
+                    dashboard_data['existing_emotional_words_data'] , 
+                    values = 'Appearance Amount', names = 'Existing-Emotional-Words',
+                    title = '<b> 4) Percentage of existing emotion words in the given dataset </b>'
+                )
+    fig.update_traces( 
+            textposition = 'inside', 
+            textfont_size = 14
+        )
     return fig
+
+
+# a function to make trust words chart figure
+def make_existing_trust_words_fig():
+    labels = dashboard_data['existing_trust_words_data']['Existing-Trust-Words']
+    values = dashboard_data['existing_trust_words_data']['Appearance Amount']
+    fig = go.Figure( data = [ go.Pie( labels = labels, values = values, hole = .6 ) ] )
+    fig.update_traces( 
+        textposition = 'inside', 
+        textfont_size = 14
+    )
+    fig.update_layout( title_text = '<b> 5) Appearence of trust words in the given dataset </b>' )
+    return fig
+
+
+# a function to show summary in table figure
+def make_summary_table_fig():
+    titles = [ key for key in dashboard_data['summary_data'] ]
+    first_column = dashboard_data['summary_data'][ titles[0] ]
+    last_column = dashboard_data['summary_data'][ titles[1] ]
+    third_column = dashboard_data['summary_data'][ titles[2] ]
+    fourth_column = dashboard_data['summary_data'][ titles[3] ]
+
+    rowEvenColor = 'lightgrey'
+    rowOddColor = 'white'
+    width = 1000
+
+    fig = go.Figure(
+                        data = [ 
+                                go.Table(   columnwidth = [0.15 * width, 0.35 * width, 0.18 * width, 0.15 * width, 0.15 * width],
+                                            header = dict( 
+                                                values = titles,
+                                                line_color = 'darkslategray',
+                                                fill_color = 'grey',
+                                                align = ['left','center'],
+                                                font = dict( color = 'white', size = 16 )
+                                            ),
+                                            cells = dict( 
+                                                values = [ first_column, last_column, third_column, fourth_column ],
+                                                line_color='darkslategray',
+                                                fill_color = [ 
+                                                                [ rowOddColor, rowEvenColor, rowOddColor, rowEvenColor, rowOddColor ] * 5 
+                                                             ],
+                                                align = ['left', 'center'],
+                                                height = 40,
+                                                font = dict( color = 'darkslategray', size = 14 )
+                                            ) 
+                                        )
+                            ] 
+                    )
+    fig.update_layout( title_text = '<b> 6) Summary </b>' )
+    return fig
+
 
 
 
 app.layout = html.Div( 
     children = [ 
-            html.H1( children = 'IR Analytics', ), 
-            html.P( children = ' My analytics ', ), 
+            html.H1( children = 'Assignment in Information Retrieval Course', style = { 'text-align': 'center' } ), 
             dcc.Graph( figure = make_trust_words_fig() ),
             dcc.Graph( figure = make_emotional_pairs_fig() ),
             dcc.Graph( figure = make_emotional_words_fig() ),
-            dcc.Graph( figure = make_pie_charts() )
+            dcc.Graph( figure = make_existing_emotion_words_fig() ),
+            dcc.Graph( figure = make_existing_trust_words_fig() ),
+            dcc.Graph( figure = make_summary_table_fig() )
         ]
  )
 
--- summary_data_manager.py
+++ summary_data_manager.py
@@ -13,7 +13,6 @@ summary_root_dir = 'Resources/Summary.json'
 summary = load_json_file( summary_root_dir )
 
 
-
 def make_trust_words_fig_data():
     x = [ key for key in summary['Trust-Words'] ]
     y = [ val for key, val in summary['Trust-Words'].items() ]
@@ -43,11 +42,62 @@ def make_emotional_words_fig_data():
     return df_temp
 
 
+def make_all_existing_emotion_words():
+    x = [ key for key, val in summary['Emotional-Words'].items() if val > 0]
+    y = [ summary['Emotional-Words'][key] for key in x ]
+    existing_emotion_words_data = { 'Existing-Emotional-Words': x, 'Appearance Amount': y}
+    existing_emotion_words_data = pd.DataFrame( existing_emotion_words_data )
+    return existing_emotion_words_data
+
+
+def make_all_existing_trust_words():
+    x = [ key for key, val in summary['Trust-Words'].items() if val > 0]
+    y = [ summary['Trust-Words'][key] for key in x ]
+    existing_emotion_words_data = { 'Existing-Trust-Words': x, 'Appearance Amount': y}
+    existing_emotion_words_data = pd.DataFrame( existing_emotion_words_data )
+    return existing_emotion_words_data
+
+
+def make_summary_table_fig():
+    columns_titles = ['<b>Subject</b>', '<b>Exisiting In The Dataset</b>', '<b>Total</b>', '<b>Percentage</b>']
+
+    emw_len = len(summary['Emotional-Words'])
+    trw_len = len(summary['Trust-Words'])
+    tep_len = len(summary['Trust-Emotional-Pairs'])
+
+    emw = [ key for key, val in summary['Emotional-Words'].items() if val > 0 ]
+    trw = [ key for key, val in summary['Trust-Words'].items() if val > 0 ]
+
+    
+    subjects = [ 'Files Numbers', 'Sentences Numbers', 'Emotion Words', 'Trust Words', 'Trust-Emotional Pairs' ]
+    eitds = [ 773, 24174, len(emw), len(trw), tep_len ]
+    total = [ 773, 24174, emw_len, trw_len, (emw_len * trw_len) ]
+
+    emwp = ( len(emw) / emw_len ) * 100
+    trwp = ( len(trw) / trw_len ) * 100
+    tepp = ( tep_len / (emw_len * trw_len)) * 100
+
+    percentage = [ '100%', '100%', "{:.2f}%".format(emwp), "{:.2f}%".format(trwp), "{:.2f}%".format(tepp)]
+
+    summary_data = {
+        columns_titles[0]: subjects,
+        columns_titles[1]: eitds,
+        columns_titles[2]: total,
+        columns_titles[3]: percentage
+    }
+
+    summary_data = pd.DataFrame( summary_data )
+    return summary_data
+
+
 def load_dashboard_data( ):
     dashboard_data = { }
     dashboard_data['trust_words_data'] = make_trust_words_fig_data()
     dashboard_data['trust_emotional_pairs_data'] = make_trust_emotional_pairs_fig_data()
     dashboard_data['emotional_words_data'] = make_emotional_words_fig_data()
+    dashboard_data['existing_emotional_words_data'] = make_all_existing_emotion_words()
+    dashboard_data['existing_trust_words_data'] = make_all_existing_trust_words()
+    dashboard_data['summary_data'] = make_summary_table_fig()
     return dashboard_data
 
 
